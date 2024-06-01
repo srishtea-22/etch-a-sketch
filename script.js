d07e9c68173bbcc0696a6564c7f3849e6e14c09d
shristy <shristychaudhary4679@gmail.com>
2024-06-01 20:34:46 +0530

    added buttons with css

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const container = document.getElementById("container");
 
-let dimension = 5;
+let dimension = 16;
 
 function makeGrid(rows){
-   
+    console.log('doing rows');
     for (let i = 0; i < rows; i++){
         let rowcell = document.createElement('div');
         rowcell.classList.add("rowcell")
+        rowcell.setAttribute("style", "border: 1px solid lightgrey")
         
         for (let j = 0; j <= rows; j++){
             let colcell = document.createElement('div');
