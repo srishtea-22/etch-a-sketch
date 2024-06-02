545df140cf7d373a25eb1040d2082e69832204ed
shristy <shristychaudhary4679@gmail.com>
2024-06-02 11:11:15 +0530

    added hover effect

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ const container = document.getElementById("container");
 let dimension = 16;
 
 function makeGrid(rows){
-    console.log('doing rows');
     for (let i = 0; i < rows; i++){
         let rowcell = document.createElement('div');
-        rowcell.classList.add("rowcell");
+        rowcell.className = ("rowcell");
         
         for (let j = 0; j < rows; j++){
             let colcell = document.createElement('div');
-            colcell.classList.add("colcell");
+            colcell.className = ("colcell");
             rowcell.appendChild(colcell);
         }
         container.appendChild(rowcell);
@@ -33,3 +32,8 @@ newGrid.addEventListener("click", () =>{
    makeGrid(newDimension);
 })
 
+container.addEventListener("mouseover", (event) => {
+  if (event.target.className === "colcell") {
+    event.target.style.background = "black";
+  }
+});
