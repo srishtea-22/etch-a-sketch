285bca650d07c68a3ff99a44b51bc5a60e1110e4
shristy <shristychaudhary4679@gmail.com>
2024-06-02 11:00:56 +0530

    fixed grid

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ function makeGrid(rows){
     console.log('doing rows');
     for (let i = 0; i < rows; i++){
         let rowcell = document.createElement('div');
-        rowcell.classList.add("rowcell")
-        rowcell.setAttribute("style", "border: 1px solid lightgrey")
+        rowcell.classList.add("rowcell");
         
-        for (let j = 0; j <= rows; j++){
+        for (let j = 0; j < rows; j++){
             let colcell = document.createElement('div');
             colcell.classList.add("colcell");
-            colcell.setAttribute("style", "border: 1px solid lightgrey")
             rowcell.appendChild(colcell);
         }
         container.appendChild(rowcell);
